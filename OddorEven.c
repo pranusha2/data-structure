0e294deb5aaec4b7cf5fdddb575b8190770eaee2
pranusha2 <113331369+pranusha2@users.noreply.github.com>
2022-09-14 10:19:26 +0530

    Create OddorEven.c

---
+++ OddorEven.c
@@ -0,0 +1,20 @@
+# include < stdio.h >
+int  main( )
+{
+int  num ;
+printf(" Enter a Number : ") ;
+scanf("%d ",& num) ;
+if (  num  % 2 == 0 )
+{
+printf("\n Entered Number is Even.") ;
+}
+else
+{
+printf("\n Entered Number is Odd.") ;
+}
+return ( 0 ) ;
+}
+
+Output:
+Enter a number:15
+Entered number is odd.
